d5a3a45a288acc0f01ee72bd64ffda48e252707b
agile-seo-dev <agile-seo-dev@users.noreply.github.com>
2024-11-21 21:03:47 +0000

    Adding to Azure to get the GitHub yaml

---
+++ src/Web.Mdw/Pages/Services.cshtml.cs
@@ -0,0 +1,42 @@
+using Application.Pages.Models;
+using Application.Settings;
+using Application.Tenants.Services;
+using Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Razor.Models;
+using Razor.ViewModels.Pages;
+
+namespace Web.Mdw.Pages;
+
+public class ServicesModel : LayoutModel
+{
+	private readonly IWebHostEnvironment _env;
+	private readonly TenantSettings _options;
+
+	public ServicesModel(
+		IRouteService routeService,
+		ITenantService tenantService,
+		IWebHostEnvironment env,
+		IOptions<TenantSettings> options)
+		: base(tenantService, routeService)
+	{
+		_env = env;
+		_options = options.Value;
+	}
+
+	public override async Task<IActionResult> OnGetAsync(CancellationToken cancellationToken)
+	{
+		var pageKey = _routeService.GetPageKey();
+		var siteKey = _routeService.GetSiteKey();
+
+		var tenantId = _tenantService.GetTenantId();
+
+		var tenant = TenantData.Create(_options);
+		var page = tenant.Sites.First(x => x.Key == siteKey).Pages.First(x => x.Key == pageKey);
+
+		PageViewModel = ServicesPageViewModel.From(page);
+
+		return PageViewModel == null ? NotFound() : Page();
+	}
+}
